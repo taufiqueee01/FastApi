d05700ae78a1f38406ed2f8aae62b7f1db8e2477
taufiqueee01 <taufiqueahmad9494@gmail.com>
2026-09-15 08:37:04 +0530

    Added password hatching & fixed bug

--- 03_AuthHub Api/main.py
+++ 03_AuthHub Api/main.py
@@ -4,6 +4,8 @@ from sqlalchemy import select
 from db import get_db, Base, engine
 from models import UserModel
 from schemas import UserCreate
+from security import hash_password, verify_password 
+
 
 Base.metadata.create_all(engine)
 app = FastAPI()
@@ -13,7 +15,7 @@ app = FastAPI()
 @app.post("/auth/register")
 def register(user: UserCreate, db: Session = Depends(get_db)):
 
-    registeration = UserModel(username=user.username, password=user.password)
+    registeration = UserModel(username=user.username,password=hash_password(user.password))
 
     user_cheak = db.execute(
         select(UserModel).where(
@@ -35,20 +37,35 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 
 # 2. Login
 @app.post("/auth/login")
-def login(user: UserCreate, db: Session = Depends(get_db)):
+def login(user: UserCreate,db: Session = Depends(get_db)):
+    
     try:
+        
         user_cheak = db.execute(
             select(UserModel).where(
-                UserModel.username
-                == user.username  # Ye UserModel.title db ke data se compare krr rha hai
+                UserModel.username== user.username  # Ye UserModel.title db ke data se compare krr rha hai
             )
         ).scalar_one_or_none()
+        
+        
+        if user_cheak is None:
+            raise HTTPException(
+                status_code=401,
+                detail="User not found, Please register"
+            )
+        verify = verify_password(user.password,user_cheak.password) 
+        
+        if not verify:
+            raise HTTPException(
+                status_code=401,
+                detail="Please enter the correct username or password.."
+            )           
+        return f"Successfully login as {user.username},{user_cheak.password}"
+    
     except Exception:
         db.rollback()
         raise
 
-    if user_cheak:
-        return f"Successfully Login as {user.username}"
 
 
 # 3. Get Current User
